284e54e151ec567beeab7f38dcea6d518c5db145
Yurii Mosin <m.yuriy@icloud.com>
2024-11-02 18:44:07 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
 // write code here
+const ul = document.querySelector('ul');
+const li = document.querySelectorAll('li');
+
+for (const i of li) {
+  const span = document.createElement('span');
+
+  i.prepend(span);
+  span.prepend(span.nextSibling);
+}
+
+ul.addEventListener('click', (e) => {
+  if (e.target.tagName === 'SPAN') {
+    const targ = e.target.nextSibling;
+
+    if (targ.tagName === 'UL') {
+      targ.style.display = targ.style.display === 'none' ? 'block' : 'none';
+    }
+  }
+});
